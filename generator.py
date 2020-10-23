c699d6f41e70a8a9f4668d2847def997df91c0dc
JoaoVitor <jvmelop@gmail.com>
2020-10-23 11:14:38 -0300

    done

---
+++ generator.py
@@ -0,0 +1,43 @@
+import json
+
+managers = {}
+watchers = {}
+
+with open('source_file_2.json') as f:
+  data = json.load(f)
+
+
+for project in data:
+  projectName = project["name"] 
+
+  for managerName in project["managers"]:
+    if not managerName in managers:
+      managers[managerName] = []
+    projPrior = {
+      "name": projectName,
+      "priority": project["priority"]
+    }
+    managers[managerName].append(projPrior)
+
+  for watcherName in project["watchers"]:
+    if not watcherName in watchers:
+      watchers[watcherName] = []
+    projPrior = {
+      "name": projectName,
+      "priority": project["priority"]
+    }
+    watchers[watcherName].append(projPrior)
+
+for mName in managers:
+  managerProjects = managers[mName]
+  managers[mName] = sorted(managerProjects, key=lambda k: k['priority'])
+
+for wName in watchers:
+  watcherProjects = watchers[wName]
+  watchers[wName] = sorted(watcherProjects, key=lambda k: k['priority'])
+
+with open('managers.json', 'w') as mjf:
+  json.dump(managers, mjf)
+
+with open('watchers.json', 'w') as wjf:
+  json.dump(watchers, wjf)
